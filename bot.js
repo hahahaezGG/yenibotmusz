f7b5105d7e121540dc280db033e59d4488cb662f
hahahaezGG <43311837+hahahaezGG@users.noreply.github.com>
2018-10-28 19:26:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ const log = message => {
 };
 
 client.on("ready", () => {
-  client.user.setGame(prefix + "yardım | SVO TR | 7-24 ") 
+  client.user.setGame(prefix + "yardım | xRaZ | 7-24 ") 
   console.log("Bağlandım!")   
 });
 
