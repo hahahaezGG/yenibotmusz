4867c4846cec8d5bae89257716ab8cafcf43ef70
hahahaezGG <43311837+hahahaezGG@users.noreply.github.com>
2018-11-30 22:25:53 +0300

    Update avatarım.js

--- komutlar/avatarım.js
+++ komutlar/avatarım.js
@@ -1,21 +1,16 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js');qwewq
 exports.run = function(client, message, args) {
-message.channel.sendEmbed(new Discord.RichEmbed()
+message.channel.sendEmbed(new Discord.RichEmbed()qewqewq
 .setDescription(`Avatarınız:`qwewqeqwe)
 .setImage(`${message.author.avatarURL} `)
-.setColor(0xf7dc46));
+.setColor(0xf7dc46));qweqwew
    }
 
-
+qweqwwqe
 exports.conf = {
  enabled: true,
- guildOnly: false,
- aliases: [],
- permLevel: 0
-};
-
-exports.help = {
- name: 'avatarım',
- description: 'Avatarınızı Atar ',
- usage: 'avatarım'
-};
+ guildOnly: false,qwwq
+ aliases: [],eq
+};wqe
+ee
+ description: 'Avatarınızı Atar 'qwqe
